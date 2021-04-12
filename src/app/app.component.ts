ad294979594464dc2b0d602acd8a90b8c98e7c41
Alex Samari <alisamarim7@gmail.com>
2021-04-12 14:49:59 -0400

    update nav button to kendoButton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-root',
+  selector: "my-root",
   template: `
-    <h1>{{title}}</h1>
+    <h1>{{ title }}</h1>
     <div class="header-bar"></div>
     <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <button
+        kendoButton
+        look="outline"
+        routerLink="/dashboard"
+        routerLinkActive="active"
+      >
+        Dashboard
+      </button>
+      <button
+        kendoButton
+        look="outline"
+        routerLink="/heroes"
+        routerLinkActive="active"
+      >
+        Heroes
+      </button>
     </nav>
     <router-outlet></router-outlet>
   `,
-  styleUrls: ['./app.component.css']
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = "Tour of Heroes";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { HttpClientInMemoryWebApiModule, InMemoryDbService } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 // import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroSearchComponent } from './hero-search.component';
-
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HeroService } from "./hero.service";
+import { DashboardComponent } from "./dashboard.component";
+import { HeroesComponent } from "./heroes.component";
+import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroSearchComponent } from "./hero-search.component";
+import { ButtonModule } from "@progress/kendo-angular-buttons";
 @NgModule({
   imports: [
     BrowserModule,
@@ -24,8 +24,9 @@ import { HeroSearchComponent } from './hero-search.component';
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
       delay: 300,
-      passThruUnknownUrl: true
-    })
+      passThruUnknownUrl: true,
+    }),
+    ButtonModule,
   ],
   declarations: [
     AppComponent,
@@ -35,6 +36,6 @@ import { HeroSearchComponent } from './hero-search.component';
     HeroDetailComponent,
   ],
   providers: [HeroService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
