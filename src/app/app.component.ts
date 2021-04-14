43058782eb7d70be47473a476ed6b471f5040dab
Alex Samari <alisamarim7@gmail.com>
2021-04-13 20:34:54 -0400

    move app component html to its own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "my-root",
-  template: `
-    <h1>{{ title }}</h1>
-    <div class="header-bar"></div>
-    <nav>
-      <button
-        kendoButton
-        look="outline"
-        routerLink="/dashboard"
-        routerLinkActive="active"
-      >
-        Dashboard
-      </button>
-      <button
-        kendoButton
-        look="outline"
-        routerLink="/heroes"
-        routerLinkActive="active"
-      >
-        Heroes
-      </button>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: "app.component.html",
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
