f26f1663861ee74031c2f71ce9661414fbfdecdb
Alex Samari <alisamarim7@gmail.com>
2021-04-14 00:14:55 -0400

    implement kendo-appbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+interface KendoTabSelectEvent {
+  prevented: boolean;
+  index: number;
+  title: string;
+}
 
 @Component({
   selector: "my-root",
@@ -7,4 +14,12 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "Tour of Heroes";
+
+  constructor(private router: Router) {}
+
+  navigate(event: KendoTabSelectEvent): void {
+    if (!event?.title) return;
+    const route = "/" + event.title.replace(/\s+/g, "-").toLocaleLowerCase();
+    this.router.navigate([route]);
+  }
 }
