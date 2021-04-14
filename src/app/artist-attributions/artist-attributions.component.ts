9c3f17c59db3dea8bc852eae1fc19e72e11b309e
Alex Samari <alisamarim7@gmail.com>
2021-04-14 01:53:24 -0400

    add artist attributions

--- src/app/artist-attributions/artist-attributions.component.ts
+++ src/app/artist-attributions/artist-attributions.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'my-artist-attributions',
-  templateUrl: './artist-attributions.component.html',
-  styleUrls: ['./artist-attributions.component.scss']
+  selector: "my-artist-attributions",
+  templateUrl: "./artist-attributions.component.html",
+  styleUrls: ["./artist-attributions.component.scss"],
 })
 export class ArtistAttributionsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
