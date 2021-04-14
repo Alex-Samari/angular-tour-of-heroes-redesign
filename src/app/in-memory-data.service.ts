bd699bc45ab621cae5d63c24e2369420fd31e882
Alex Samari <alisamarim7@gmail.com>
2021-04-13 21:47:13 -0400

    updated hero names and add img path

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,16 +1,20 @@
 export class InMemoryDataService {
   createDb() {
     const heroes = [
-      { id: 11, name: "Mr. Nice" },
-      { id: 12, name: "Narco" },
-      { id: 13, name: "Bombasto" },
-      { id: 14, name: "Celeritas" },
-      { id: 15, name: "Magneta" },
-      { id: 16, name: "RubberMan" },
-      { id: 17, name: "Dynama" },
-      { id: 18, name: "Dr IQ" },
-      { id: 19, name: "Magma" },
-      { id: 20, name: "Tornado" },
+      { id: 11, name: "Achilles", img: "../assets/images/achilles.jpg" },
+      { id: 12, name: "Cloud Boy", img: "../assets/images/cloud-boy.jpg" },
+      {
+        id: 13,
+        name: "Illusto Girl",
+        img: "../assets/images/illusto-girl.jpg",
+      },
+      { id: 14, name: "Night Lady", img: "../assets/images/night-lday.jpg" },
+      { id: 15, name: "Robin", img: "../assets/images/robin.jpg" },
+      { id: 16, name: "Shazam", img: "../assets/images/shazam.jpg" },
+      { id: 17, name: "Super Dad", img: "../assets/images/super-dad.jpg" },
+      { id: 18, name: "Superman", img: "../assets/images/superman.jpg" },
+      { id: 19, name: "Superwoman", img: "../assets/images/superwoman.jpg" },
+      { id: 20, name: "Wingman", img: "../assets/images/wingman.jpg" },
     ];
     return { heroes };
   }
