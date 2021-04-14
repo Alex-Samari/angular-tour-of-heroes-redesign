38adb906b70dd1e390877e42a86c7e2a45de1dd4
Alex Samari <alisamarim7@gmail.com>
2021-04-14 00:29:25 -0400

    add artist-attributions route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard.component";
 import { HeroesComponent } from "./heroes.component";
 import { HeroDetailComponent } from "./hero-detail.component";
+import { ArtistAttributionsComponent } from "./artist-attributions/artist-attributions.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/dashboard", pathMatch: "full" },
   { path: "dashboard", component: DashboardComponent },
   { path: "detail/:id", component: HeroDetailComponent },
   { path: "heroes", component: HeroesComponent },
+  { path: "artist-attributions", component: ArtistAttributionsComponent },
 ];
 
 @NgModule({
